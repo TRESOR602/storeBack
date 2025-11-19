4141a7ddf4f4962b5f019d1abdb9a2030df88185
TRESOR602 <ndetresor39@gmail.com>
2025-11-19 13:51:28 +0100

    .

--- serveur.js
+++ serveur.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const mariadb=require('mariadb');
 const cors=require('cors');
-const port=4000;
+const port = process.env.PORT || 4000;
 require('dotenv').config();
 const transporter=require('./transpoter');
 const paths=require('./assets/transglobe/path');
